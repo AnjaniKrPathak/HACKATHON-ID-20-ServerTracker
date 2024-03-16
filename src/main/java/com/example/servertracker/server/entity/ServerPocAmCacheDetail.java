ad649f6255f6abc66219e1ab6f06ca98aae3e926
Anjani Kumar Pathak <anjani.pathak@netcracker.com>
2024-03-16 09:44:28 +0530

    Added Server POCAMCacheDetail Class

---
+++ src/main/java/com/example/servertracker/server/entity/ServerPocAmCacheDetail.java
@@ -0,0 +1,11 @@
+package com.example.servertracker.server.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ServerPocAmCacheDetail {
+}
