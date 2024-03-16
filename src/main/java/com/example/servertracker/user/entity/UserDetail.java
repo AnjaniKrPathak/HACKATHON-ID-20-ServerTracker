cfc4903b55b097969c2bf9d4c6964907f6f56b41
Rupesh Singh <rupesh.singh@netcracker.com>
2024-03-16 16:35:58 +0530

    Changes for password

--- src/main/java/com/example/servertracker/user/entity/UserDetail.java
+++ src/main/java/com/example/servertracker/user/entity/UserDetail.java
@@ -22,6 +22,8 @@ public class UserDetail {
     private Long id;
     @Column(name = "USER_NAME")
     private String name;
+    @Column(name = "USER_PASS")
+    private String pass;
     @Column(name = "EMAIL")
     private String email;
     @Column(name = "PROJECT")
